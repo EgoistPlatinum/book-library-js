42f7a1875c26b3f2c9503e7932804cef4202fc29
Aleksey Leonov <74103840+AlekseyLeonov031@users.noreply.github.com>
2026-01-02 23:24:28 +0500

    Added `CardList` component with styling and integrated it into `MainView` to display book search results

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -2,6 +2,7 @@ import { AbstractView } from '../../common/view.js';
 import { Header } from '../../components/header/header.js';
 import onChange from 'on-change';
 import { Search } from '../../components/search/search.js';
+import { CardList } from '../../components/card-list/card-list.js';
 
 export class MainView extends AbstractView {
   state = {
@@ -17,6 +18,9 @@ export class MainView extends AbstractView {
     this.appState = onChange(this.appState, this.appStateHook.bind(this));
     this.state = onChange(this.state, this.stateHook.bind(this));
     this.setTitle('Поиск книг');
+
+    this.mainElement = document.createElement('main');
+    this.headerInstance = new Header(this.appState);
   }
 
   appStateHook(path) {
@@ -33,6 +37,10 @@ export class MainView extends AbstractView {
       console.log(data);
       this.state.list = data.docs;
     }
+
+    if (path === 'list' || path === 'loading') {
+      this.render();
+    }
   }
 
   async loadList(q, offset) {
@@ -41,15 +49,30 @@ export class MainView extends AbstractView {
   }
 
   render() {
-    const main = document.createElement('main');
-    main.innerHTML = '';
-    main.append(new Search(this.state).render());
-    this.app.append(main);
+    this.mainElement.innerHTML = '';
+
+    this.mainElement.append(new Search(this.state).render());
+    this.mainElement.append(new CardList(this.appState, this.state).render());
+
+    const oldMain = this.app.querySelector('main');
+    if (oldMain && oldMain !== this.mainElement) {
+      oldMain.remove();
+    }
+
+    if (!this.app.contains(this.mainElement)) {
+      this.app.append(this.mainElement);
+    }
+
     this.renderHeader();
   }
 
   renderHeader() {
-    const header = new Header(this.appState).render();
+    const oldHeader = this.app.querySelector('header');
+    if (oldHeader) {
+      oldHeader.remove();
+    }
+
+    const header = this.headerInstance.render();
     this.app.prepend(header);
   }
 }
